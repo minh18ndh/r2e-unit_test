c0485602426a4a710029771cc843b39ca40813a6
Minh Nguyen <minhnguyendohoang18052003@gmail.com>
2025-04-14 16:26:39 +0700

    Update

--- MySecondMVC.Tests/obj/Debug/net8.0/MySecondMVC.Tests.AssemblyInfo.cs
+++ MySecondMVC.Tests/obj/Debug/net8.0/MySecondMVC.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySecondMVC.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c402d193a5c3cd84e08615c230577b6c5bb2b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7426fd3fa4d4ca8441c6adefde4d3d2f585f1d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySecondMVC.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySecondMVC.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
